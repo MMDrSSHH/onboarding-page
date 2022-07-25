4fd58a87a9b23e3d74de424c3596e08dea7823bc
MohammadReza Shekohi <mrshekohi079@gmail.com>
2022-07-25 13:40:17 +0430

    Onboarding component's return statement changed + collapsing animation was added

--- src/components/onboarding/Onboarding.js
+++ src/components/onboarding/Onboarding.js
@@ -16,7 +16,7 @@ const Onboarding = () => {
   const videoRef = useRef(null);
 
   useEffect(() => {
-    if (sliderRef.current !== null) {
+    if (sliderRef.current !== null && current !== data.length - 1) {
       // Trigger sliding animation
       sliderRef.current.style.transform = `translateX(-${
         (100 / (data.length - 1)) * current
@@ -41,135 +41,113 @@ const Onboarding = () => {
     setCurrent(index);
   };
 
-  // JSX for first (n - 1) slides
-  if (current !== data.length - 1) {
-    return (
-      <>
-        <div className={style["onboarding-overlay"]}>
-          <div className={style["onboarding-container"]}>
-            {/* Left side (text section) */}
-            <div className={style["onboarding-text-section"]}>
-              {/* Slider container */}
-              <div
-                className={style["text-slider"]}
-                ref={sliderRef}
-                style={{ width: `${(data.length - 1) * 100}%` }}
-              >
-                {data.slice(0, data.length - 1).map((item) => {
-                  return (
-                    <div key={item.id} className={style["slide-item"]}>
-                      <div className={style["onboarding-text"]}>
-                        <h3 className={style["text-header"]}>{item.heading}</h3>
-                        <p className={style["text-desc"]}>{item.description}</p>
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-              {/* Slider container */}
-              <div className={style["navigation-container"]}>
-                <div onClick={handleNext} style={{ zIndex: "10" }}>
-                  <Button text="NEXT" />
-                </div>
-                {/* Bullets for index navigating */}
-                <div className={style.navigation}>
-                  {data.slice(0, data.length - 1).map((item, index) => (
-                    <span
-                      onClick={() => handleNavigate(index)}
-                      key={item.id}
-                      className={`${style.bullet} ${
-                        index === current ? style.selected : ""
-                      }`}
-                    ></span>
-                  ))}
-                </div>
-                {/* Bullets for index navigating */}
-              </div>
-            </div>
-            {/* Left side (text section) */}
-            {/* Right side (video section) */}
-            <div className={style["onboarding-video-section"]}>
-              <video
-                className={style["background-video"]}
-                ref={videoRef}
-                autoPlay
-                loop
-                muted
-              >
-                <source src={data[current].video} type="video/mp4" />
-              </video>
-            </div>
-            {/* Right side (video section) */}
-            {/* Slide navigation buttons */}
-            {current > 0 ? (
-              <button
-                onClick={handlePrev}
-                className={`${style["arrow-left"]} ${style["arrow-btn"]}`}
-              >
-                <FontAwesomeIcon icon={faArrowLeft} />
-              </button>
-            ) : null}
-            <button
-              onClick={handleNext}
-              className={`${style["arrow-right"]} ${style["arrow-btn"]}`}
-            >
-              <FontAwesomeIcon icon={faArrowRight} />
-            </button>
-            {/* Slide navigation buttons */}
-          </div>
-        </div>
-      </>
-    );
-  }
-  // JSX for last slide (data item)
-  else {
-    return (
-      <>
-        <div className={style["onboarding-overlay"]}>
+  return (
+    <>
+      <div className={style["onboarding-overlay"]}>
+        <div
+          className={`${style["onboarding-container"]} ${
+            current === data.length - 1 ? style["last"] : ""
+          }`}
+        >
+          {/* Left side (text section) */}
           <div
-            className={style["onboarding-container"]}
-            style={{ width: "30%" }}
+            className={`${style["onboarding-text-section"]} ${
+              current === data.length - 1 ? style["last"] : ""
+            }`}
           >
+            {/* Slider container */}
             <div
-              className={`${style["onboarding-video-section"]} ${style.last}`}
-              style={{ width: "100%", borderRadius: "10px" }}
+              className={style["text-slider"]}
+              ref={sliderRef}
+              style={{ width: `${(data.length - 1) * 100}%` }}
             >
-              <video
-                className={style["background-video"]}
-                style={{ borderRadius: "10px" }}
-                ref={videoRef}
-                autoPlay
-                loop
-                muted
-              >
-                <source src="/videos/video-1.mp4" type="video/mp4" />
-              </video>
-              <div className={style["video-overlay"]}></div>
-              <div className={`${style["onboarding-text"]} ${style.last}`}>
-                <h3 className={`${style["text-header"]} ${style.last}`}>
-                  {data[current].heading}
-                </h3>
-                <p className={`${style["text-desc"]} ${style.last}`}>
-                  {data[current].description}
-                </p>
-              </div>
-              <Link to="/home" className={style["link-button"]}>
-                <Button text="START WRITING" />
-              </Link>
+              {data.slice(0, data.length - 1).map((item) => {
+                return (
+                  <div key={item.id} className={style["slide-item"]}>
+                    <div className={style["onboarding-text"]}>
+                      <h3 className={style["text-header"]}>{item.heading}</h3>
+                      <p className={style["text-desc"]}>{item.description}</p>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
-            {/* Slide navigation button */}
+            {/* Slider container */}
+            <div className={style["navigation-container"]}>
+              <div onClick={handleNext} style={{ zIndex: "10" }}>
+                <Button text="NEXT" />
+              </div>
+              {/* Bullets for index navigating */}
+              <div className={style.navigation}>
+                {data.slice(0, data.length - 1).map((item, index) => (
+                  <span
+                    onClick={() => handleNavigate(index)}
+                    key={item.id}
+                    className={`${style.bullet} ${
+                      index === current ? style.selected : ""
+                    }`}
+                  ></span>
+                ))}
+              </div>
+              {/* Bullets for index navigating */}
+            </div>
+          </div>
+          {/* Left side (text section) */}
+          {/* Right side (video section) */}
+          <div
+            className={`${style["onboarding-video-section"]} ${
+              current === data.length - 1 ? style["last"] : ""
+            }`}
+          >
+            {current === data.length - 1 ? (
+              <>
+                <div className={style["video-overlay"]}></div>
+                <div className={`${style["onboarding-text"]} ${style.last}`}>
+                  <h3 className={`${style["text-header"]} ${style.last}`}>
+                    {data[current].heading}
+                  </h3>
+                  <p className={`${style["text-desc"]} ${style.last}`}>
+                    {data[current].description}
+                  </p>
+                </div>
+                <Link to="/home" className={style["link-button"]}>
+                  <Button text="START WRITING" />
+                </Link>
+              </>
+            ) : null}
+            <video
+              className={style["background-video"]}
+              ref={videoRef}
+              autoPlay
+              loop
+              muted
+            >
+              <source src={data[current].video} type="video/mp4" />
+            </video>
+          </div>
+          {/* Right side (video section) */}
+          {/* Slide navigation buttons */}
+          {current > 0 ? (
             <button
               onClick={handlePrev}
               className={`${style["arrow-left"]} ${style["arrow-btn"]}`}
             >
               <FontAwesomeIcon icon={faArrowLeft} />
             </button>
-            {/* Slide navigation buttons */}
-          </div>
+          ) : null}
+          {current !== data.length - 1 ? (
+            <button
+              onClick={handleNext}
+              className={`${style["arrow-right"]} ${style["arrow-btn"]}`}
+            >
+              <FontAwesomeIcon icon={faArrowRight} />
+            </button>
+          ) : null}
+          {/* Slide navigation buttons */}
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 };
 
 export default Onboarding;
