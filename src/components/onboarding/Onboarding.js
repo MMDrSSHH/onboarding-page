daf5afd24a879133bfb50ef3990bfd236899b5a7
MohammadReza Shekohi <mrshekohi079@gmail.com>
2022-07-25 11:37:06 +0430

    sliding animation was added to the text section and new video was added

--- src/components/onboarding/Onboarding.js
+++ src/components/onboarding/Onboarding.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import style from "../../styles/onboarding.module.css";
 import Button from "../button/Button";
@@ -8,6 +8,18 @@ import { Link } from "react-router-dom";
 
 const Onboarding = () => {
   const [current, setCurrent] = useState(0);
+  const sliderRef = useRef(null);
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (sliderRef.current !== null) {
+      sliderRef.current.style.transform = `translateX(-${
+        (100 / (data.length - 1)) * current
+      }%)`;
+    }
+    videoRef.current.load();
+  }, [current]);
+
   const handleNext = () => {
     setCurrent((prev) => prev + 1);
   };
@@ -26,13 +38,21 @@ const Onboarding = () => {
         <div className={style["onboarding-overlay"]}>
           <div className={style["onboarding-container"]}>
             <div className={style["onboarding-text-section"]}>
-              <div className={style["onboarding-text"]}>
-                <h3 className={style["text-header"]}>
-                  {data[current].heading}
-                </h3>
-                <p className={style["text-desc"]}>
-                  {data[current].description}
-                </p>
+              <div
+                className={style["text-slider"]}
+                ref={sliderRef}
+                style={{ width: `${(data.length - 1) * 100}%` }}
+              >
+                {data.slice(0, data.length - 1).map((item) => {
+                  return (
+                    <div key={item.id} className={style["slide-item"]}>
+                      <div className={style["onboarding-text"]}>
+                        <h3 className={style["text-header"]}>{item.heading}</h3>
+                        <p className={style["text-desc"]}>{item.description}</p>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
               <div className={style["navigation-container"]}>
                 <div onClick={handleNext} style={{ zIndex: "10" }}>
@@ -52,8 +72,13 @@ const Onboarding = () => {
               </div>
             </div>
             <div className={style["onboarding-video-section"]}>
-              <video className={style["background-vidoe"]} autoPlay loop>
-                <source src="/videos/video-1.mp4" type="video/mp4" />
+              <video
+                className={style["background-video"]}
+                ref={videoRef}
+                autoPlay
+                loop
+              >
+                <source src={data[current].video} type="video/mp4" />
               </video>
             </div>
             {current > 0 ? (
@@ -89,18 +114,16 @@ const Onboarding = () => {
               style={{ width: "100%", borderRadius: "10px" }}
             >
               <video
-                className={style["background-vidoe"]}
+                className={style["background-video"]}
                 style={{ borderRadius: "10px" }}
+                ref={videoRef}
                 autoPlay
                 loop
               >
                 <source src="/videos/video-1.mp4" type="video/mp4" />
               </video>
               <div className={style["video-overlay"]}></div>
-              <div
-                className={`${style["onboarding-text"]} ${style.last}`}
-                style={{ width: "100%" }}
-              >
+              <div className={`${style["onboarding-text"]} ${style.last}`}>
                 <h3 className={`${style["text-header"]} ${style.last}`}>
                   {data[current].heading}
                 </h3>
--- src/data/data.js
+++ src/data/data.js
@@ -4,29 +4,34 @@ export const onboardingData = [
     heading: "Editing made easy",
     description:
       "Get detailed feedback on your writing and apply suggestions with just one click.",
+    video: "/videos/video-1.mp4",
   },
   {
     id: "2",
     heading: "Become a better writer",
     description:
       "See your feedback organized into categories that help you indentify where your writing is strong and where you can make it stronger.",
+    video: "/videos/video-2.mp4",
   },
   {
     id: "3",
     heading: "Go beyond grammar",
     description:
       "Make your writing clear, compelling, andconcise with suggestions customized to your writing goal.",
+    video: "/videos/video-1.mp4",
   },
   {
     id: "4",
     heading: "Set your goals",
     description:
       "Grammarly's feedback based on your objectives, your target audience, your writing style, and more.",
+    video: "/videos/video-2.mp4",
   },
   {
     id: "5",
     heading: "Enjoy\n the Grammarly Editor",
     description:
       "You're ready to start writing! You can also copy and paste or upload some existing text to the Grammarly Editor.",
+    video: "/videos/video-1.mp4",
   },
 ];
